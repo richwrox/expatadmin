3615399ccf07cb031263a3cb5261a1577fef71eb
Ricahrd <rarhin@techneutron.com>
2021-11-23 01:50:18 +0000

    Update-3

--- app/Http/Livewire/EmployeeList.php
+++ app/Http/Livewire/EmployeeList.php
@@ -15,6 +15,6 @@ class EmployeeList extends Component
 
     public function render()
     {
-        return view('livewire.employee-list',['employeeList'=> \DB::table('institution_users AS I')->join('patients AS P','P.token','=','I.user_token')->select('P.*')->where('I.institution_token','=','6936f353-e6ff-4d99-95e9-3011d3584f1d')->orderBy('P.created_at','DESC')->paginate(3) ]);
+        return view('livewire.employee-list',['employeeList'=> \DB::table('institution_users AS I')->join('patients AS P','P.token','=','I.user_token')->select('P.*')->where('I.institution_token','=',\Session::get('partnerToken'))->orderBy('P.created_at','DESC')->paginate(3) ]);
     }
 }
